a7325e48a66e0d0de8e21d1a3bbe2c35f92db731
mlee0210 <lee.cs.michael@gmail.com>
2018-06-14 19:59:37 -0700

    Complete: linkedList
    All tests are passing:
    Passed: should have a head and tail
    Passed: should have methods named "addToTail", "removeHead", and "contains"
    Passed: should designate a new tail when new nodes are added
    Passed: should remove the head from the list when removeHead is called
    Passed: should return the value of the former head when removeHead is called
    Passed: should contain a value that was added
    Passed: should not contain a value that was removed

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,62 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // if the current head and tail is not set, create new object and set 
+    // head and tail to new object:
+    if(this.head === null && this.tail === null){
+      this[value] = new Node(value);
+      this.tail = this[value];
+      this.head = this[value];
+    } else {
+      // Create a new node by calling 'Node' function, store in list
+      this[value] = new Node(value);
+
+      // Find current tail:
+      var currentTail = this.tail;
+
+      // On current tail, assign 'next' property to new node:
+      currentTail.next = this[value];
+      
+      // Set the list's tail to the new variable
+      this.tail = this[value];
+
+
+    }
+
   };
 
   list.removeHead = function() {
+    //create a variable that will store current head
+    var removedObject = this.head;
+
+    //create variable to store the current head's value:
+    var removedValue = removedObject.value; 
+
+    //find out what the next node the current head points to
+    //make head equal next node
+    this.head = this.head.next;
+
+    //delete variable from list
+    delete this[removedValue];
+
+    //return variable  
+    return removedObject.value;
   };
 
   list.contains = function(target) {
+  //declare result variable which will be a boolean
+  var result = false;
+  
+  //create an array of object keys or use for..in loop through list
+  for (var key in list) {
+    //check if element.value is equal to target
+    if (parseInt(key) === target) {
+      //if true, set result to equal true;
+      result = true;
+    }
+  }
+  //return result
+  return result;
   };
 
   return list;
@@ -27,3 +77,5 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// var node1 = Node(12)
