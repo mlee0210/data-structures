26b54aec18a2a0adcbf134bcbcec68f12324fabf
mlee0210 <lee.cs.michael@gmail.com>
2018-06-15 21:47:36 -0700

    Working: Hash Table
    Test Passed: should have methods named "insert", "remove", and "retrieve
    Test Passed: should store values that were inserted
    Test Passed: should not contain values that were not inserted
    Test Passed: should overwrite values that have the same key
    Test Failed: should not contain values that were removed
    Test Failed: should handle hash function collisions

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,17 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
 };
 
 
@@ -22,5 +25,27 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/*
+
+Creates a new hash table:
+var ht = new HashTable()
+
+ht._storage will contain the following functions:
+
+1) each(callback)
+  - For every element within the hash table, call 'callback' on that element
+
+2) get(index)
+  - Input: takes in an index
+  - Output: returns the value within the hash table at that index
+
+3) set(index, value)
+  - Input: an index and a value
+  - Outputs: nothing
+  - Within the hash table, it stores the value at the given index within 
+    the storage array.
+
+*/
 
 
