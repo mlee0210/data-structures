4aaf195a468b02439a58f33467e785939db93dc7
mlee0210 <lee.cs.michael@gmail.com>
2018-06-15 20:24:34 -0700

    Completed: Set
    Passed Test: should have methods named "add", "contains", and "remove"
    Passed Test: should add values to a set
    Passed Test: should remove values from a set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = 1; // fix me
+  set.contents = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // If the set does not include the item, add it.
+  if(!this.contains(item)){
+    this.contents.push(item);
+  }
+
 };
 
 setPrototype.contains = function(item) {
+  // If the set contains the item, return true
+  if(this.contents.includes(item)){
+    return true;
+  } else {
+  // Otherwise, return false.
+    return false; 
+  }
 };
 
 setPrototype.remove = function(item) {
+  // If the set includes the item, remove it.
+  if(this.contains(item)){
+    this.contents.splice(this.contents.indexOf(item), 1);
+  }
 };
 
 /*
