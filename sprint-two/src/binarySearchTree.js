b889723e63e08adcd2139f45ab7244797094e2e7
mlee0210 <lee.cs.michael@gmail.com>
2018-06-16 16:14:42 -0700

    Working: Binary Search Tree
    Passed Test: should have methods named "insert", "contains", and "depthFirstLog ‣
    Passed Test: should insert values at the correct location in the tree ‣
    Failed Test: should have a working "contains" method
    Failed Test: should execute a callback on every value in a tree using "depthFirstLog"

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,53 @@
 var BinarySearchTree = function(value) {
+
+  var binaryTree = Object.create(binaryTreeMethods);
+  
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  
+  return binaryTree;
 };
 
+var isGreaterThan = function(newNode, parentNode) {
+  var val1 = newNode.value;
+  var val2 = parentNode.value;
+  if(val1 > val2) {
+    if(parentNode.right){
+      isGreaterThan(newNode, parentNode.right);
+    } else {
+      parentNode.right = newNode; 
+    }
+  } else if(val1 < val2) {
+    if(parentNode.left) {
+      isGreaterThan(newNode, parentNode.left);
+    } else {
+      parentNode.left = newNode;
+    }
+  } 
+  
+};
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+var binaryTreeMethods = {
+  'insert' : function(value) {
+  
+  //create a new node
+  var treeNode = BinarySearchTree(value);
+  
+  //store object keys
+  var treeKeys = this;
+
+  //compare the value of new node to the value of first key
+  isGreaterThan(treeNode, treeKeys);
+
+
+
+  },
+
+  'contains' : function(value) {
+  },
+
+  'depthFirstLog' : function(fn) {
+  }
+  
+}
